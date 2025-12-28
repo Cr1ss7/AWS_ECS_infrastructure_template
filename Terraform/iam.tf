27cb2d13d5ecbdc85d006d8462be5c3da33a935e
Cristian Pineda <pineda.cristian@hotmail.com>
2025-12-27 23:01:57 -0600

    IAM role ECS modification

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -20,3 +20,8 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy_attachment"
     role = aws_iam_role.ecs_execution_role.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "rds_full_role_policy_attachment" {
+    role = aws_iam_role.ecs_execution_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
